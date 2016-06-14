f83ba0e22468141c443efccddc0a8bb9515f1e84
brettcao <benteng.cao@mozzos.com>
2016-06-14 11:56:27 +0800

    dev

---
+++ src/NLPTool.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brett
+ * Date: 2016/6/13
+ * Time: 23:14
+ */
+
+namespace Seek\NLPTool;
+
+
+use Illuminate\Config\Repository;
+
+class NLPTool
+{
+    /**
+     * @var Repository
+     */
+    protected $config;
+
+    /**
+     * @var array
+     */
+    protected $notifications = [];
+
+    /**
+     * Toastr constructor.
+     * @param Repository $config
+     */
+    public function __construct(Repository $config)
+    {
+        $this->config = $config;
+    }
+}
